c70095290a6bf3d9fbd9cbda886b3cadae974f4e
Ivan M <insectbob@outlook.com>
2022-09-13 18:09:37 +0200

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,10 @@
+export default {
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['~/plugins/composition-api.ts'],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ['@nuxt/typescript-build', 'nuxt-typed-vuex'],
+}
---
+++ pages/composition.vue
@@ -0,0 +1,28 @@
+<template>
+  <div :class="$style.test">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from '@vue/composition-api'
+
+export default defineComponent({
+  setup(_props, { root }) {
+    const email = computed(() => root.$accessor.email)
+    const setEmail = () => {
+      root.$accessor.setEmail('test@email.com')
+    }
+
+    return {
+      email,
+      setEmail,
+    }
+  },
+})
+</script>
+
+<style lang="postcss" module>
+.test {
+}
+</style>
